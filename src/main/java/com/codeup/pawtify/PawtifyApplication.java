2b192460188fb05c1333d8d8eb9610644516b08b
emmadejong <dejong.l.emma@gmail.com>
2018-04-26 15:12:30 -0500

    init commit

---
+++ src/main/java/com/codeup/pawtify/PawtifyApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.pawtify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawtifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawtifyApplication.class, args);
+	}
+}
