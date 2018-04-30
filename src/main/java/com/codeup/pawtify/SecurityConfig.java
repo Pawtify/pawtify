f7f9d0678ba280a8ec3ac2c57a557a7ac1d79a7c
emmadejong <dejong.l.emma@gmail.com>
2018-04-30 13:19:42 -0500

    added the UserDetailsLoader class import to the file

--- src/main/java/com/codeup/pawtify/SecurityConfig.java
+++ src/main/java/com/codeup/pawtify/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.codeup.pawtify;
 
+import com.codeup.pawtify.services.UserDetailsLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
