845309c5dbca1f6b545d68f404ccf9fb7f7af750
emmadejong <dejong.l.emma@gmail.com>
2018-04-30 13:10:04 -0500

    created the UserWithRoles class with security methods

---
+++ src/main/java/com/codeup/pawtify/models/UserWithRoles.java
@@ -0,0 +1,39 @@
+package com.codeup.pawtify.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserWithRoles extends User implements UserDetails{
+    public UserWithRoles(User user) {
+        super(user);  // Call the copy constructor defined in User
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String roles = ""; // Since we're not using the authorization part of the component
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
