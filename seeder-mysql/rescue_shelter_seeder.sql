e3fc26ea418e4ef243f1d723c60b2ae70de10616
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-05-03 20:37:29 -0500

    worked on rs-portal

--- seeder-mysql/rescue_shelter_seeder.sql
+++ seeder-mysql/rescue_shelter_seeder.sql
@@ -1,9 +1,19 @@
 USE pawtify_db;
 
+
 INSERT INTO rescue_shelter(name, address, phone, user_id) VALUES
   ('Animal Care Services', '4710 TX-151, San Antonio, TX 78227', '+12102074738', 1),
   ('San Antonio Humane Society', '4804 Fredericksburg Rd, San Antonio, TX 78229', '+12102267461', 2),
-  ('Animal Defense League of Texas', '11300 Nacogdoches Rd, San Antonio, TX 78217', '+12106551481', 3);
+  ('Animal Defense League of Texas', '11300 Nacogdoches Rd, San Antonio, TX 78217', '+12106551481', 3),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 4),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 5),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 6),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 7),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 8),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 9),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 10),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 11),
+  ('Shelter of Caring Hearts', '4600 South San Pablo Rd, San Antonio, TX 78201', '+12108675309', 12);
 
 # 1. Animcal Care Services
 # 2. San Antonio Humane Society
