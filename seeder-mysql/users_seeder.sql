77bc4759d0192f47079fae56e5e9d10b0738b463
emmadejong <dejong.l.emma@gmail.com>
2018-05-04 14:41:01 -0500

    added rs-portal functionality

--- seeder-mysql/users_seeder.sql
+++ seeder-mysql/users_seeder.sql
@@ -1,8 +1,8 @@
 USE pawtify_db;
 
-INSERT INTO users(full_name, phone, username, email, password) VALUES
-  ('emma dejong', '+13059008059', 'edejong', 'edejong@gmail.com','edejong'),
-  ('laura prochaska', '+13059009059', 'lalepro', 'llprochaska@gmail.com', 'lalepro'),
-  ('bettin trejo', '+13059008059', 'btrejo', 'btrejo@gmail.com', 'btrejo');
+INSERT INTO users(full_name, phone, username, email, password, shelter_id) VALUES
+  ('emma dejong', '+13059008059', 'edejong', 'edejong@gmail.com','edejong', 1),
+  ('laura prochaska', '+13059009059', 'lalepro', 'llprochaska@gmail.com', 'lalepro', 2),
+  ('bettin trejo', '+13059008059', 'btrejo', 'btrejo@gmail.com', 'btrejo', 3);
 
 # SELECT * FROM users;
