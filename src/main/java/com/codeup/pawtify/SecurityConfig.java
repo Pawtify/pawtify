221bd7f16869f4349c0e5d0da9e0f01eddbaba18
Bettina Trejo <bettina.trejo12@gmail.com>
2018-05-03 23:41:32 -0500

    working on functional navbar

--- src/main/java/com/codeup/pawtify/SecurityConfig.java
+++ src/main/java/com/codeup/pawtify/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.codeup.pawtify;
 
 import com.codeup.pawtify.services.UserDetailsLoader;
+import com.sun.corba.se.impl.presentation.rmi.ExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -32,13 +33,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 /* Login configuration for pa*/
                 .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/pawtification/") // user's home page, it can be any URL
+                    .defaultSuccessUrl("/pawtification/")// user's home page, it can be any URL
                     .permitAll() // Anyone can go to the login page
                 /* Logout configuration */
                 .and()
@@ -54,10 +56,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorizeRequests()
                     .antMatchers("/animals/create", "/animals/{id}/edit")
                     .hasAuthority("rolestaff")
-                .and()
-                .authorizeRequests()
-                .antMatchers("/pawtification/")
-                .hasAuthority("roleadopter");
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/pawtification/")
+//                .hasAuthority("roleadopter")
+
+        ;
 
 
 
