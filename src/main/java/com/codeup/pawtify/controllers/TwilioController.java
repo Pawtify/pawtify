beff04a7bd4a3dba81e1d64fab17e2032f339ea7
Bettina Trejo <bettina.trejo12@gmail.com>
2018-05-05 19:13:58 -0500

    added redirection for pawtification

--- src/main/java/com/codeup/pawtify/controllers/TwilioController.java
+++ src/main/java/com/codeup/pawtify/controllers/TwilioController.java
@@ -1,19 +1,19 @@
-package com.codeup.pawtify.controllers;
-
-import com.codeup.pawtify.daos.PawtificationRepository;
-import com.codeup.pawtify.daos.UsersRepository;
-import com.codeup.pawtify.services.TwilioService;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class TwilioController {
-    private TwilioService twilioSvc;
-    private PawtificationRepository pawtificationRepository;
-    private UsersRepository usersRepository;
-
-    public TwilioController(TwilioService twilioSvc, PawtificationRepository pawtificationRepository, UsersRepository usersRepository) {
-        this.twilioSvc = twilioSvc;
-        this.pawtificationRepository = pawtificationRepository;
-        this.usersRepository = usersRepository;
-    }
-}
+//package com.codeup.pawtify.controllers;
+//
+//import com.codeup.pawtify.daos.PawtificationRepository;
+//import com.codeup.pawtify.daos.UsersRepository;
+//import com.codeup.pawtify.services.TwilioService;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class TwilioController {
+//    private TwilioService twilioSvc;
+//    private PawtificationRepository pawtificationRepository;
+//    private UsersRepository usersRepository;
+//
+//    public TwilioController(TwilioService twilioSvc, PawtificationRepository pawtificationRepository, UsersRepository usersRepository) {
+//        this.twilioSvc = twilioSvc;
+//        this.pawtificationRepository = pawtificationRepository;
+//        this.usersRepository = usersRepository;
+//    }
+//}
