9b745918316ed2acfd49eb8f51376138447b2cbb
emmadejong <dejong.l.emma@gmail.com>
2018-05-07 14:05:30 -0500

    commented out to rul local host

--- src/main/java/com/codeup/pawtify/services/TwilioService.java
+++ src/main/java/com/codeup/pawtify/services/TwilioService.java
@@ -1,32 +1,32 @@
-package com.codeup.pawtify.services;
-
-import com.codeup.pawtify.models.User;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-import org.springframework.stereotype.Service;
-
-
-@Service
-public class TwilioService {
-
-    private String twiliosid = "ACcdf7ce35f8d546df26cc502263850156";
-    private String twiliotoken = "661e59735feab0ac177e61be6315dcf5";
-    private String twilionumber = "8304200317";
-
-    //in case we can send customized messages
-    public String customMessage() {
-        return null;
-    }
-
-    public Message sendSMS(User user) {
-        Twilio.init(this.twiliosid, this.twiliotoken);
-
-        PhoneNumber phoneNumberTo = new PhoneNumber(user.getPhone());
-        PhoneNumber phoneNumberFrom = new PhoneNumber(twilionumber);
-
-        Message message = Message.creator(phoneNumberTo, phoneNumberFrom, "You've received a Pawtification Match! Click the link to meet your match: " + mediaURL()).create();
-
-        return message;
-    } //can this return void?
-}
+//package com.codeup.pawtify.services;
+//
+//import com.codeup.pawtify.models.User;
+//import com.twilio.Twilio;
+//import com.twilio.rest.api.v2010.account.Message;
+//import com.twilio.type.PhoneNumber;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class TwilioService {
+//
+//    private String twiliosid = "ACcdf7ce35f8d546df26cc502263850156";
+//    private String twiliotoken = "661e59735feab0ac177e61be6315dcf5";
+//    private String twilionumber = "8304200317";
+//
+//    //in case we can send customized messages
+//    public String customMessage() {
+//        return null;
+//    }
+//
+//    public Message sendSMS(User user) {
+//        Twilio.init(this.twiliosid, this.twiliotoken);
+//
+//        PhoneNumber phoneNumberTo = new PhoneNumber(user.getPhone());
+//        PhoneNumber phoneNumberFrom = new PhoneNumber(twilionumber);
+//
+//        Message message = Message.creator(phoneNumberTo, phoneNumberFrom, "You've received a Pawtification Match! Click the link to meet your match: " + mediaURL()).create();
+//
+//        return message;
+//    } //can this return void?
+//}
