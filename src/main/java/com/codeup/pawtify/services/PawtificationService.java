278fcf999412247c34a6e7c2f005015da5da3a89
emmadejong <dejong.l.emma@gmail.com>
2018-05-07 16:46:52 -0500

    reset this local commit

--- src/main/java/com/codeup/pawtify/services/PawtificationService.java
+++ src/main/java/com/codeup/pawtify/services/PawtificationService.java
@@ -24,3 +24,6 @@ public class PawtificationService extends TwilioService {
         return null; //only send/use this method if there is a match, when does this message run? every time a pawtification is created
     }
 }
+
+
+//test
