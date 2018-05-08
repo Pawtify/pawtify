a5c6266283f5b67335be8a2c4260d5dabc40b2d0
emmadejong <dejong.l.emma@gmail.com>
2018-05-08 14:38:20 -0500

    removed twilio controller file

--- src/main/java/com/codeup/pawtify/daos/AnimalRepository.java
+++ src/main/java/com/codeup/pawtify/daos/AnimalRepository.java
@@ -1,10 +1,15 @@
 package com.codeup.pawtify.daos;
 
 import com.codeup.pawtify.models.Animal;
+import com.codeup.pawtify.models.CatBreed;
+import com.codeup.pawtify.models.DogBreed;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnimalRepository extends CrudRepository<Animal, Long> {
-
+    List<Animal> findAllByAgeAndSizeAndColorAndGenderAndCatBreed(String age, String size, String color, String gender, CatBreed catBreed);
+    List<Animal> findAllByAgeAndSizeAndColorAndGenderAndDogBreed(String age, String size, String color, String gender, DogBreed dogBreed);
 }
