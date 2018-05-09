13e7a64d2af9d6d1a6b9dd856174e4fdd4a2e348
emmadejong <dejong.l.emma@gmail.com>
2018-05-09 10:21:39 -0500

    commented out queries that emma is working on so the app can run

--- src/main/java/com/codeup/pawtify/daos/PawtificationRepository.java
+++ src/main/java/com/codeup/pawtify/daos/PawtificationRepository.java
@@ -1,5 +1,6 @@
 package com.codeup.pawtify.daos;
 
+import com.codeup.pawtify.models.Animal;
 import com.codeup.pawtify.models.Pawtification;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -12,7 +13,8 @@ public interface PawtificationRepository extends CrudRepository<Pawtification, L
     @Query(nativeQuery = true, value = " SELECT * FROM pawtifications WHERE user_id = ?1")
     List<Pawtification> findPawtificationByUserId(long id);
 
-    @Query(nativeQuery = true, value = "SELECT pawtification.id, animals.name FROM animals INNER JOIN pawtifications ON pawtifications.size = animals.size ");
-
-    @Query(nativeQuery = true, value = " select * from animal where cat_breed_id in (select cat_breed_id from pawtification where user_id in (select id from user where user = ?1")
+//    @Query(nativeQuery = true, value = "SELECT animals.id FROM animals INNER JOIN pawtifications ON pawtifications.size = animals.size AND pawtifications.color = animals.color AND pawtifications.gender = animals.gender AND pawtifications.age = animals.age AND (pawtifications.cat_breed_id = animals.cat_breed_id OR pawtifications.dog_breed_id = animals.dog_breed_id)")
+//    List<Animal> fin
+//   @Query(nativeQuery = true, value = "SELECT * FROM animals where animals.cat_breed_id IN (SELECT pawtifications.cat_breed_id FROM pawtifications WHERE user_id IN (SELECT id FROM users WHERE id = ?1")
+//    List<Animal>
 }
