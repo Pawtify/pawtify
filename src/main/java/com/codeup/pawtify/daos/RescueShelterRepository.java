6afce3b048505a405ad7ed947b1530d5e909680a
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-05-15 14:21:50 -0500

    clean up code

--- src/main/java/com/codeup/pawtify/daos/RescueShelterRepository.java
+++ src/main/java/com/codeup/pawtify/daos/RescueShelterRepository.java
@@ -1,11 +1,9 @@
 package com.codeup.pawtify.daos;
 
 import com.codeup.pawtify.models.RescueShelter;
-//import com.codeup.pawtify.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RescueShelterRepository extends CrudRepository<RescueShelter, Long> {
-
 }
