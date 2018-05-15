fc35de2579e08ee70f25ebdaf54f4b89f391c757
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-05-15 14:21:19 -0500

    src/main/java/com/codeup/pawtify/daos/PawtificationRepository.java

--- src/main/java/com/codeup/pawtify/daos/PawtificationRepository.java
+++ src/main/java/com/codeup/pawtify/daos/PawtificationRepository.java
@@ -6,20 +6,14 @@ import com.codeup.pawtify.models.Pawtification;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface PawtificationRepository extends CrudRepository<Pawtification, Long> {
-
+    // METHODS FOR TWILIO USED IN THE PAWTIFICATION
     List<Pawtification> findAllByAgeAndSizeAndColorAndGenderAndCatBreed(String age, String size, String color, String gender, CatBreed catBreed);
     List<Pawtification> findAllByAgeAndSizeAndColorAndGenderAndDogBreed(String age, String size, String color, String gender, DogBreed dogBreed);
-
+    //METHODS FOR THE PAWTIFICATION CONTROLLER, USED IN THE GET MAPPING PAWITIFICATION CREATE METHOD
     @Query(nativeQuery = true, value = " SELECT * FROM pawtifications WHERE user_id = ?1")
     List<Pawtification> findPawtificationByUserId(long id);
-
-//    @Query(nativeQuery = true, value = "SELECT animals.id FROM animals INNER JOIN pawtifications ON pawtifications.age = animals.age AND pawtifications.size = animals.size AND pawtifications.color = animals.color AND pawtifications.gender = animals.gender AND (pawtifications.cat_breed_id = animals.cat_breed_id OR pawtifications.dog_breed_id = animals.dog_breed_id)")
-//    List<Animal> findAnimalsByPawtificationAndUser(long id);
-//   @Query(nativeQuery = true, value = "SELECT * FROM animals where animals.cat_breed_id IN (SELECT pawtifications.cat_breed_id FROM pawtifications WHERE user_id IN (SELECT id FROM users WHERE id = ?1")
-//    List<Animal>
 }
